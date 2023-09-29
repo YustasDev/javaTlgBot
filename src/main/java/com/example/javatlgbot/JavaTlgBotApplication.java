aaf85e69953f7ad73eef2eb1fa65929c1ab86e11
vitaly.gusev <vitaly.gusev@progforce.com>
2023-09-29 15:22:25 +0300

    init 0.1

---
+++ src/main/java/com/example/javatlgbot/JavaTlgBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatlgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTlgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTlgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javatlgbot/JavaTlgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatlgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTlgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
